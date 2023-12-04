e2a0b143525aa3fa895a3c5c0dfa3df1cddea6a5
JakBiel <jakbiel1@gmail.com>
2023-12-04 13:21:17 +0100

    Optimization of the downloader

--- zip_data_docker_compose/zip_data_downloader_app/zip_data_downloader.py
+++ zip_data_docker_compose/zip_data_downloader_app/zip_data_downloader.py
@@ -2,94 +2,65 @@ import requests
 import zipfile
 import os
 import psycopg2
+from psycopg2.extras import execute_values
 import csv
 import sys
 
 
-url = 'https://wyszukiwarka.gunb.gov.pl/pliki_pobranie/wynik_zgloszenia_2020_up.zip'
-response = requests.get(url)
+def download_and_unpack_zip(url, local_zip_path, extract_to_folder):
+    print("Rozpoczynanie pobierania pliku ZIP...")
+    response = requests.get(url)
+    with open(local_zip_path, 'wb') as file:
+        file.write(response.content)
+    print("Plik ZIP pobrany. Rozpoczynanie rozpakowywania...")
+    with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to_folder)
+    print("Rozpakowywanie zakończone.")
 
-# Saving the file locally
-with open('zip_data.zip', 'wb') as file:
-    file.write(response.content)
 
-# Unpacking the .zip file
-with zipfile.ZipFile('zip_data.zip', 'r') as zip_ref:
-    zip_ref.extractall('unpacked_zip_data_files')
-
-
-# Connection parameters
-host = "zip_data_postgres_db"
-db_name = os.environ.get('POSTGRES_DB')
-user = os.environ.get('POSTGRES_USER')
-password = os.environ.get('POSTGRES_PASSWORD')
-
-def check_duplicate(cursor, row):
-    # Check if a record with the same value in the first column already exists
-    cursor.execute("SELECT * FROM reporting_results2020 WHERE numer_ewidencyjny_system = %s", (row[0],))
-    existing_record = cursor.fetchone()
-
-    if existing_record:
-        # If a record is found, compare all columns
-        return all([existing_record[i] == row[i] for i in range(len(row))])
-    else:
-        # If no record is found, it's not a duplicate
-        return False
-
-def load_data_from_csv(file_path):
-    # Connect to your PostgreSQL database
+def load_data_from_csv(file_path, db_params):
+    print("Łączenie z bazą danych...")
     conn = psycopg2.connect(
-        dbname=db_name, 
-        user=user, 
-        password=password, 
-        host=host
+        dbname=db_params['db_name'],
+        user=db_params['user'],
+        password=db_params['password'],
+        host=db_params['host']
     )
+    print("Połączenie z bazą danych nawiązane.")
     cursor = conn.cursor()
 
-
-    # Set the maximum field size to the maximum integer value
+    # Zwiększ limit rozmiaru pola
     csv.field_size_limit(sys.maxsize)
 
-    # Open the CSV file
+    print("Rozpoczynanie wczytywania danych z CSV...")
     with open(file_path, 'r', encoding='utf-8') as file:
         reader = csv.reader(file, delimiter='#')
+        next(reader)  # Skip the header
 
-        # Skip the first row (header)
-        next(reader)
-
-        iterator = 0
-        iterator_of_bad = 0
-
+        batch = []
         for row in reader:
-            if len(row) != 26:  # Ensure the row has exactly 26 columns
-                if len(row) > 2:
-                    print(f"Warning: row with incorrect number of columns: {len(row)}", row)
-                print(f"----{iterator_of_bad}")
-                iterator_of_bad=iterator_of_bad+1
-                continue
+            if len(row) != 26 or any(len(value) > 131071 for value in row):
+                continue  # Pomijanie wierszy z błędnymi danymi
 
-            row_is_valid = True  # flag showing if the row is correct
+            batch.append(tuple(row))
 
-            for value in row:
-                if len(value) > 131071:
-                    print(f"Zbyt długa wartość, liczba znaków:{len(value)} dla kolumny: ", row, " ", value)
-                    row_is_valid = False  # flag setting as False if the row is invalid
-                    break
-                
+            if len(batch) >= 1000:
+                execute_values(cursor, """
+                    INSERT INTO reporting_results2020 (
+                        numer_ewidencyjny_system, numer_ewidencyjny_urzad, data_wplywu_wniosku_do_urzedu, 
+                        nazwa_organu, wojewodztwo_objekt, obiekt_kod_pocztowy, miasto, terc, cecha, cecha2,
+                        ulica, ulica_dalej, nr_domu, kategoria, nazwa_zam_budowlanego, rodzaj_zam_budowlanego, 
+                        kubatura, stan, jednostki_numer, obreb_numer, numer_dzialki, 
+                        numer_arkusza_dzialki, nazwisko_projektanta, imie_projektanta, 
+                        projektant_numer_uprawnien, projektant_pozostali
+                    ) VALUES %s
+                """, batch)
+                conn.commit()
+                batch = []
 
-            if not row_is_valid:  # Checking if the row is correct. If not, a new iteraion of "for row in reader" is beginning
-                print(f"----{iterator_of_bad}")
-                iterator_of_bad=iterator_of_bad+1
-                continue
-
-            print(f"{iterator}")
-            iterator=iterator+1
-
-            if check_duplicate(cursor, row):
-                print(f"Duplicate record found, skipping insertion")
-                continue
-
-            cursor.execute("""
+        
+        if batch:
+            execute_values(cursor, """
                 INSERT INTO reporting_results2020 (
                     numer_ewidencyjny_system, numer_ewidencyjny_urzad, data_wplywu_wniosku_do_urzedu, 
                     nazwa_organu, wojewodztwo_objekt, obiekt_kod_pocztowy, miasto, terc, cecha, cecha2,
@@ -97,15 +68,63 @@ def load_data_from_csv(file_path):
                     kubatura, stan, jednostki_numer, obreb_numer, numer_dzialki, 
                     numer_arkusza_dzialki, nazwisko_projektanta, imie_projektanta, 
                     projektant_numer_uprawnien, projektant_pozostali
-                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """, row)
+                ) VALUES %s
+            """, batch)
+            conn.commit()
+
+    cursor.close()
+    conn.close()
+    print("Wczytywanie danych zakończone.")
 
 
-    # Commit changes and close the connection
+def remove_duplicates(db_params):
+    print("Usuwanie duplikatów...")
+    conn = psycopg2.connect(
+        dbname=db_params['db_name'],
+        user=db_params['user'],
+        password=db_params['password'],
+        host=db_params['host']
+    )
+    cursor = conn.cursor()
+    cursor.execute("""
+        DELETE FROM reporting_results2020
+        WHERE ctid IN (
+            SELECT ctid
+            FROM (
+                SELECT ctid, ROW_NUMBER() OVER (
+                    PARTITION BY numer_ewidencyjny_system
+                    ORDER BY (SELECT 1)
+                ) AS rn
+                FROM reporting_results2020
+            ) t
+            WHERE rn > 1
+        );
+    """)
     conn.commit()
     cursor.close()
     conn.close()
+    print("Duplikaty usunięte.")
 
-# Replace with your CSV file path
-csv_file_path = 'unpacked_zip_data_files/wynik_zgloszenia_2020_up.csv'
-load_data_from_csv(csv_file_path)
+
+def main():
+    url = 'https://wyszukiwarka.gunb.gov.pl/pliki_pobranie/wynik_zgloszenia_2020_up.zip'
+    local_zip_path = 'zip_data.zip'
+    extract_to_folder = 'unpacked_zip_data_files'
+    csv_file_path = 'unpacked_zip_data_files/wynik_zgloszenia_2020_up.csv'
+
+    db_params = {
+        'host': "zip_data_postgres_db",
+        'db_name': os.environ.get('POSTGRES_DB'),
+        'user': os.environ.get('POSTGRES_USER'),
+        'password': os.environ.get('POSTGRES_PASSWORD')
+    }
+
+    download_and_unpack_zip(url, local_zip_path, extract_to_folder)
+    load_data_from_csv(csv_file_path, db_params)
+    remove_duplicates(db_params)
+
+
+if __name__ == "__main__":
+    print("Rozpoczynanie głównego procesu...")
+    main()
+    print("Zakończono główny proces.")
