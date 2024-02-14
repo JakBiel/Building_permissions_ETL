00566f25b1c81f2943649f7d3751b17d4ed3f9ec
JakBiel <jakbiel1@gmail.com>
2024-02-14 13:38:10 +0100

    The aggregate finally works fine BUT still the data upload process to be fixed

--- dags/aggregates_python.py
+++ dags/aggregates_python.py
@@ -71,7 +71,10 @@ def apply_additional_criteria(df, db_params):
     batch_iterator = 0
     all_data = []  # Store all data for new DataFrame
 
+    #print("Connecting to the database...")
     conn = psycopg2.connect(**db_params)
+    #print("Connection established.")
+    
     cursor = conn.cursor()
 
     # Define the column names for the new DataFrame
@@ -92,7 +95,11 @@ def apply_additional_criteria(df, db_params):
             batch.append(row_to_tuple(row))
             all_data.append(row_to_tuple(row))
             if len(batch) >= batch_size:
+                
+                #print(f"Starting to process batch {batch_iterator}...")
                 manage_batch(cursor, conn, batch, batch_size, batch_iterator)
+                #print(f"Batch {batch_iterator} processing completed.")
+
                 batch.clear()
                 batch_iterator += 1
         
@@ -133,16 +140,26 @@ def row_to_tuple(row):
 
 def manage_batch(cursor, conn1, batch, batch_size, batch_iterator):
     """Manage batch insertions to the database."""
-    execute_values(cursor, """
-        INSERT INTO reporting_results2020 (
-            numer_ewidencyjny_system, numer_ewidencyjny_urzad, data_wplywu_wniosku_do_urzedu, 
-            nazwa_organu, wojewodztwo_objekt, obiekt_kod_pocztowy, miasto, terc, cecha, cecha2,
-            ulica, ulica_dalej, nr_domu, kategoria, nazwa_zam_budowlanego, rodzaj_zam_budowlanego, 
-            kubatura, stan, jednostki_numer, obreb_numer, numer_dzialki, 
-            numer_arkusza_dzialki, nazwisko_projektanta, imie_projektanta, 
-            projektant_numer_uprawnien, projektant_pozostali
-        ) VALUES %s
-    """, batch, page_size=batch_size)
+    #print(f"Loading batch {batch_iterator} to the database...")
+    #print(f"Loading batch {batch_iterator} to the database with {len(batch)} records...")
+    #for i, record in enumerate(batch):
+    #    print(f"Record {i}: {record}")
+
+    try:
+        execute_values(cursor, """
+            INSERT INTO reporting_results2020 (
+                numer_ewidencyjny_system, numer_ewidencyjny_urzad, data_wplywu_wniosku_do_urzedu, 
+                nazwa_organu, wojewodztwo_objekt, obiekt_kod_pocztowy, miasto, terc, cecha, cecha2,
+                ulica, ulica_dalej, nr_domu, kategoria, nazwa_zam_budowlanego, rodzaj_zam_budowlanego, 
+                kubatura, stan, jednostki_numer, obreb_numer, numer_dzialki, 
+                numer_arkusza_dzialki, nazwisko_projektanta, imie_projektanta, 
+                projektant_numer_uprawnien, projektant_pozostali
+            ) VALUES %s
+        """, batch, page_size=batch_size)
+        #print("Batch executed successfully")
+    except Exception as e:
+        print(f"Error executing batch: {e}")
+    #print("Pre-ready - batch number: {batch_iterator}, number of records: {len(batch)}")
     conn1.commit()
     print(f"Loaded a full batch to the database, batch number: {batch_iterator}, number of records: {len(batch)}")
 
@@ -189,12 +206,19 @@ def load_data_from_csv_to_db(file_path, db_params, ti):
         latest_date = 0
         latest_date_before_convertion = 0
 
-    print(f"UWAGA: przed konwersją, latest_date ma wartość: {latest_date_before_convertion} ")
-    print(f"UWAGA: aktualna wartość latest_date wynosi: {latest_date}")
-    filtered_df = filter_data_on_date(file_path, latest_date)
-    validation_df = apply_additional_criteria(filtered_df, db_params)
+    #DEBUGGING PRINTS:
+    #print(f"UWAGA: przed konwersją, latest_date ma wartość: {latest_date_before_convertion} ")
+    #print(f"UWAGA: aktualna wartość latest_date wynosi: {latest_date}")
 
-    print(f"UWAGA: liczba zliczonych, źle skonwertowanych dat: {wrong_text_to_date_iterator}")
+    #print("Starting to filter data on date...")
+    filtered_df = filter_data_on_date(file_path, latest_date)
+    #print("Data filtering completed.")
+
+    #print("Starting to apply additional criteria...")
+    validation_df = apply_additional_criteria(filtered_df, db_params)
+    #print("Applying additional criteria completed.")
+
+    #print(f"UWAGA: liczba zliczonych, źle skonwertowanych dat: {wrong_text_to_date_iterator}")
 
     cursor.close()
     conn.close()
@@ -202,7 +226,7 @@ def load_data_from_csv_to_db(file_path, db_params, ti):
 
 def main_of_unzipped_data_uploader_and_validation(ti):
     # Path to the CSV file containing data to be validated
-    csv_file_path = 'unpacked_zip_data_files/wynik_zgloszenia_2020_up.csv'
+    csv_file_path = 'unpacked_zip_data_files/wynik_zgloszenia_2022_up.csv'
 
     # Database connection parameters
     db_params = {
@@ -228,9 +252,11 @@ def main_of_unzipped_data_uploader_and_validation(ti):
 
     ti.xcom_push(key='last_success_date', value=datetime.utcnow().isoformat())
 
+    print("Data upload and validation completed.")
+
 def main_of_zip_data_downloader(ti):
     """Main function for ZIP data downloader."""
-    url = 'https://wyszukiwarka.gunb.gov.pl/pliki_pobranie/wynik_zgloszenia_2020_up.zip'
+    url = 'https://wyszukiwarka.gunb.gov.pl/pliki_pobranie/wynik_zgloszenia_2022_up.zip'
     local_zip_path = 'zip_data.zip'
     extract_to_folder = 'unpacked_zip_data_files'
     download_and_unpack_zip(url, local_zip_path, extract_to_folder)
@@ -244,11 +270,95 @@ def main_of_aggregates_creation(ti):
     
     HOST='host.docker.internal'
     engine = create_engine(f'postgresql://{postgres_user}:{postgres_password}@{HOST}:5432/{postgres_db}')
-    
-    # Execute SQL query and load results into DataFrame
-    query = "SELECT * FROM public.reporting_results2020"  # Change to appropriate SQL query
+
+    # Execute SQL query to select records from the last 3 months
+    query = """
+    SELECT * FROM public.reporting_results2020
+    WHERE data_wplywu_wniosku_do_urzedu::timestamp >= current_date - INTERVAL '3 months';
+    """
     df = pd.read_sql_query(query, engine)
+
+    df['data_wplywu_wniosku_do_urzedu'] = pd.to_datetime(df['data_wplywu_wniosku_do_urzedu'], errors='coerce')
+    df['terc'] = pd.to_numeric(df['terc'], errors='coerce').fillna(0).astype(int)
+
+
     # Aggregate creation logic here
+
+    # Prepare date-related data
+    today = pd.Timestamp(datetime.now())
+
+    # Convert 'data_wplywu_wniosku_do_urzedu' to datetime and filter data for the last 3, 2, and 1 month(s)
+    df_last_3m = df
+    df_last_2m = df[df['data_wplywu_wniosku_do_urzedu'] >= today - pd.DateOffset(months=2)]
+    df_last_1m = df[df['data_wplywu_wniosku_do_urzedu'] >= today - pd.DateOffset(months=1)]
+
+    aggregate3m = aggregate_creator(df_last_3m, "3m")
+    aggregate2m = aggregate_creator(df_last_2m, "2m")
+    aggregate1m = aggregate_creator(df_last_1m, "1m")
+
+    #TEMPORARILY COMMENTED
+    # # Save the ordered DataFrame to a CSV file, ensuring 'injection_date' is the second column
+
+    summary_aggregate = merge_aggregates(aggregate3m,aggregate2m,aggregate1m)
+
+    final_aggregate = correct_aggregates_column_order_plus_injection_date(summary_aggregate)
+    #aggregate3m1 = correct_aggregates_column_order_plus_injection_date(aggregate3m)
+
+    path_to_save = '/opt/airflow/desktop/aggregate_result.csv'
+    final_aggregate.to_csv(path_to_save, index=False)
+
+    print(f"Aggregate has been saved to: {path_to_save}")
+
+def aggregate_creator(df, prefix):
+    aggregate = pd.pivot_table(df, index=['terc'], columns=['kategoria', 'rodzaj_zam_budowlanego'], 
+                               aggfunc='size', fill_value=0)
+    
+    # Ensure 'terc' becomes a column if it's part of the index
+    if 'terc' not in aggregate.columns:
+        aggregate.reset_index(inplace=True)
+
+    # Modify column names except for 'terc'
+    if isinstance(aggregate.columns, pd.MultiIndex):
+        # Flatten MultiIndex if necessary and prepend prefix
+        aggregate.columns = [f"{prefix}_{'_'.join(col).rstrip('_')}" if col[0] != 'terc' else col[0] for col in aggregate.columns.values]
+    else:
+        # Prepend prefix to column names except for 'terc'
+        aggregate.columns = [f"{prefix}_{col}" if col != 'terc' else col for col in aggregate.columns]
+
+    return aggregate
+
+def merge_aggregates(aggregate3m, aggregate2m, aggregate1m):
+
+    merged_aggregate = pd.merge(aggregate3m, aggregate2m, on='terc', how='outer', suffixes=('_3m', '_2m'))
+    merged_aggregate = pd.merge(merged_aggregate, aggregate1m, on='terc', how='outer')
+
+    for col in merged_aggregate.columns:
+        if col not in ['terc', 'injection_date'] and merged_aggregate[col].dtype == float:
+            merged_aggregate[col] = merged_aggregate[col].fillna(0).astype(int)
+
+    return merged_aggregate
+
+
+
+
+
+
+def correct_aggregates_column_order_plus_injection_date(aggregate_0):
+
+    # Adding 'injection_date' after ensuring 'terc' is a column
+    aggregate_0['injection_date'] = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] + ' UTC'
+
+    # Prepare ordered columns list with 'terc' and 'injection_date' at the beginning
+    ordered_columns = ['terc', 'injection_date'] + [col for col in aggregate_0.columns if col not in ['terc', 'injection_date']]
+
+    # Apply the ordered columns to the DataFrame
+    aggregate_ordered = aggregate_0[ordered_columns]
+
+    aggregate_ordered = aggregate_ordered.rename(columns={'terc': 'unit_id'})
+
+    return aggregate_ordered
+    
+
 
 # DAG definition
 with DAG(
@@ -260,23 +370,24 @@ with DAG(
         catchup=False,  # no catching-up if scheduled dag failed
 ) as dag:
 
-    zip_data_downloader_task = PythonOperator(
-        task_id='zip_data_downloader_task',
-        python_callable=main_of_zip_data_downloader,
-        dag=dag
-    )
-
-    uzipped_data_uploader_and_validation_task = PythonOperator(
-        task_id='uzipped_data_uploader_and_validation_task',
-        python_callable=main_of_unzipped_data_uploader_and_validation,
-        dag=dag
-    )
-
-    # aggregates_creation_task = PythonOperator(
-    #     task_id='aggregates_creation_task',
-    #     python_callable=main_of_aggregates_creation,
+    # zip_data_downloader_task = PythonOperator(
+    #     task_id='zip_data_downloader_task',
+    #     python_callable=main_of_zip_data_downloader,
     #     dag=dag
     # )
 
+    # uzipped_data_uploader_and_validation_task = PythonOperator(
+    #     task_id='uzipped_data_uploader_and_validation_task',
+    #     python_callable=main_of_unzipped_data_uploader_and_validation,
+    #     dag=dag
+    # )
+
+    aggregates_creation_task = PythonOperator(
+        task_id='aggregates_creation_task',
+        python_callable=main_of_aggregates_creation,
+        dag=dag
+    )
+
 # Task dependencies
-zip_data_downloader_task >> uzipped_data_uploader_and_validation_task #>> aggregates_creation_task
+#zip_data_downloader_task >> uzipped_data_uploader_and_validation_task >> aggregates_creation_task
+aggregates_creation_task
